70a7bdf814f491067b0bc5cbd5631454ec2fa405
AbhinavBharti1 <abhinavbharti1000@gmail.com>
2024-01-06 22:02:43 +0530

    chapter 2

---
+++ link list/doublyLLcreation.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class doublyLLcreation
+{
+    static class Node{
+        int data ;
+        Node next;
+        Node prev ;
+        Node(int data)
+        {
+            this.data = data;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    Node head = null;
+    Node tail = null;
+
+    public void creation()
+    {
+        int data,n;
+        Scanner sc = new Scanner(System.in);
+        do{
+        System.out.print("Enter data = ");
+        data = sc.nextInt();
+        Node new_node = new Node(data);
+        if(head==null){
+            head=new_node;
+            tail=new_node;
+        }
+        else{
+            head.prev = new_node ;
+            new_node.next = head ;
+            head = new_node;
+        }
+        System.out.print("Do you want to continue press 1 = ");
+        n=sc.nextInt();
+    }
+    while(n==1);
+
+    }
+    public void traverse()
+    { Node temp=head;
+        if(head==null)
+        {
+            System.out.print("link list is not available");
+        }
+        else{
+            while(temp!=null)
+            {
+                System.out.print(temp.data + " ");
+                temp=temp.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        doublyLLcreation li = new  doublyLLcreation();
+        li.creation();
+        li.traverse();
+    }
+}
+
---
+++ link list/doublyLLinsertion.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+public class doublyLLinsertion
+{
+    static class Node{
+        int data ;
+        Node next;
+        Node prev ;
+        Node(int data)
+        {
+            this.data = data;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    Node head = null;
+    Node tail = null;
+
+    public void creation()
+    {
+        int data,n,m,p;
+        Scanner sc = new Scanner(System.in);
+        do{
+        System.out.print("Enter data = ");
+        data = sc.nextInt();
+        Node new_node = new Node(data);
+        if(head==null){
+            head=new_node;
+            tail=new_node;
+        }
+        else{
+            System.out.print("enter 1 to insert at beginning, enter 2 to insert in middle , enter 3 to insert at end");
+            m= sc.nextInt();
+            switch (m) {
+                case 1:
+                    new_node.next=head;
+                    head.prev = new_node;
+                    head=new_node;
+                    break;
+                case 2:
+                   System.out.println("enter position of node to be inserted");
+                   p = sc.nextInt();
+                   Node temp1=head;
+                   Node temp2 = temp1.next;
+                   for(int i = 0; i<(p-1);i++){
+                    temp1= temp2;
+                    temp2=temp2.next;
+                   }
+                   new_node.next=temp2;
+                   new_node.prev= temp1;
+                   temp1.next=new_node;
+                   temp2.prev= new_node;
+                    break;
+                 case 3:                   
+                    {
+                       tail.next=new_node;
+                       new_node.prev = tail;
+                       tail = new_node;
+                    }
+            }
+        }
+        System.out.print("Do you want to continue press 1 = ");
+        n=sc.nextInt();
+    }
+    while(n==1);
+
+    }
+    public void traverse()
+    { Node temp=head;
+        if(head==null)
+        {
+            System.out.print("link list is not available");
+        }
+        else{
+            while(temp!=null)
+            {
+                System.out.print(temp.data + " ");
+                temp=temp.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        doublyLLinsertion li = new  doublyLLinsertion();
+        li.creation();
+        li.traverse();
+    }
+}
+
